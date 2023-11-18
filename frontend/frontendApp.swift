134379f218b88bc8724df6da8d4e2458bbc3c9f3
Frank Guglielmo <frankieg1610@gmail.com>
2023-11-17 21:11:58 -0800

    Initial Commit

---
+++ frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Frank Guglielmo on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
