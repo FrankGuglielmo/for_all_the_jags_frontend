46f2df874c523a4ca2c40512bc7efdf383da31d8
clarejw <cjwilson6@dons.usfca.edu>
2023-11-19 04:44:44 -0800

    Can view individual location info

--- frontend/Views/LocationCardView.swift
+++ frontend/Views/LocationCardView.swift
@@ -56,11 +56,11 @@ struct LocationsScrollView: View {
 }
 
 let locations = [
-        Location(id: UUID(), latitude: 37.7749, longitude: -122.4194, name: "Haven's Coffee", priceRange: "$$", description: "Artisan coffee and pastries", locationType: .cafe),
-        Location(id: UUID(), latitude: 51.5074, longitude: -0.1278, name: "Grand Library", priceRange: "Free", description: "Historic library with vast collections", locationType: .library),
-        Location(id: UUID(), latitude: 40.7128, longitude: -74.0060, name: "TechSpace Office", priceRange: "$$$", description: "Modern coworking space", locationType: .office),
-        Location(id: UUID(), latitude: 48.8566, longitude: 2.3522, name: "Champs Park", priceRange: "Free", description: "Expansive park with scenic views", locationType: .park),
-        Location(id: UUID(), latitude: -33.8688, longitude: 151.2093, name: "Harbor Side Cafe", priceRange: "$$", description: "Cafe with a view of the harbor", locationType: .cafe)
+        Location(id: UUID(), latitude: 37.7749, longitude: -122.4194, name: "Haven's Coffee", priceRange: "$$", description: "Artisan coffee and pastries", locationType: .cafe, wifi: true, freeWifi: false, busyness: 1, comfort: 2, noise: 0),
+        Location(id: UUID(), latitude: 51.5074, longitude: -0.1278, name: "Grand Library", priceRange: "Free", description: "Historic library with vast collections", locationType: .library, wifi: true, freeWifi: true, busyness: 0, comfort: 2, noise: 0),
+        Location(id: UUID(), latitude: 40.7128, longitude: -74.0060, name: "TechSpace Office", priceRange: "$$$", description: "Modern coworking space", locationType: .office, wifi: true, freeWifi: false, busyness: 0, comfort: 0, noise: 1),
+        Location(id: UUID(), latitude: 48.8566, longitude: 2.3522, name: "Champs Park", priceRange: "Free", description: "Expansive park with scenic views", locationType: .park, wifi: false, freeWifi: false, busyness: 2, comfort: 1, noise: 2),
+        Location(id: UUID(), latitude: -33.8688, longitude: 151.2093, name: "Harbor Side Cafe", priceRange: "$$", description: "Cafe with a view of the harbor", locationType: .cafe, wifi: true, freeWifi: false, busyness: 1, comfort: 2, noise: 1)
     ]
 
 #Preview {
