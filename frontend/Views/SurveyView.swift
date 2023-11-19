73b44560ddb13788a07981ff1e4cdd07abf2ca74
clarejw <cjwilson6@dons.usfca.edu>
2023-11-19 07:16:25 -0800

    Survey works but need locs

--- frontend/Views/SurveyView.swift
+++ frontend/Views/SurveyView.swift
@@ -7,21 +7,44 @@
 
 import SwiftUI
 
-struct SurveyResult {
-    var busy: Int
-    var noisy: Int
-    var comfy: Int
-    var wifi: Int
-    var parking: Int
+//struct SurveyResult {
+//    var busy: Int
+//    var noisy: Int
+//    var comfy: Int
+//    var wifi: Int
+//    var parking: Int
+//    var timestamp: Date
+//    var place : String
+//}
+
+struct SurveyResult: Codable {
+    var busy_level: Int
+    var comfort_level: Int
+    var wifi_situation: Int
+    var noise_level: Int
+    var parking_situation: Int
     var timestamp: Date
-    var place : String
+    var location: String
 }
+
+
+
 
 struct SurveyView : View{
     @Environment(\.dismiss) var dismiss
     let location: Location?
     
-    @State private var surveyResult = SurveyResult(busy: -1, noisy: -1, comfy: -1, wifi: -1, parking: -1, timestamp: Date(), place: "")
+    @State private var surveyResult = SurveyResult(
+        busy_level: -1,
+        comfort_level: -1,
+        wifi_situation: -1,
+        noise_level: -1,
+        parking_situation: -1,
+        timestamp: Date(),
+        location: "" // You need to set this to the correct location ID
+    )
+
+
     
     var body: some View {
         ScrollView {
@@ -79,11 +102,9 @@ struct SurveyView : View{
                     Divider()
                 }
                 Button(action: {
-                    //TODO: BACKEND TINGS, but smth like POST(surveyResult), no?
                     surveyResult.timestamp = Date()
-                    surveyResult.place = location!.name
-                    print(surveyResult)
-                    dismiss()
+                    surveyResult.location = location?.name ?? "Unknown Location"
+                    submitSurvey()
                 }) {
                     Text("Submit")
                         .frame(minWidth: 0, maxWidth: .infinity)
@@ -97,17 +118,60 @@ struct SurveyView : View{
 
             }
         }
+    
+    func submitSurvey() {
+        guard let locationId = location?.id else { return }
+
+        guard let url = URL(string: "http://localhost:8000/surveys/") else {
+            print("Invalid URL")
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let encoder = JSONEncoder()
+        
+        // Custom date formatter
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX") // POSIX locale to ensure consistent formatting
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0) // UTC timezone
+        encoder.dateEncodingStrategy = .formatted(dateFormatter)
+
+        guard let jsonData = try? encoder.encode(surveyResult) else {
+            print("Failed to encode survey data")
+            return
+        }
+
+        let task = URLSession.shared.uploadTask(with: request, from: jsonData) { data, response, error in
+            if let error = error {
+                print("Error submitting survey: \(error)")
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 {
+                print("Survey submitted successfully")
+            } else {
+                print("Failed to submit survey")
+            }
+        }
+        task.resume()
+    }
+
+
+    
     func updateSurveyResult(quality: String, with rating: Int){
         if(quality == "busyness"){
-            surveyResult.busy = rating
+            surveyResult.busy_level = rating
         } else if (quality == "noise level"){
-            surveyResult.noisy = rating
+            surveyResult.noise_level = rating
         } else if (quality == "comfort level"){
-            surveyResult.comfy = rating
+            surveyResult.comfort_level = rating
         } else if (quality == "wifi quality"){
-            surveyResult.wifi = rating
+            surveyResult.wifi_situation = rating
         } else if (quality == "parking"){
-            surveyResult.parking = rating
+            surveyResult.parking_situation = rating
         }
     }
     
